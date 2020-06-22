a53732fc60ff4a4e6bea06a5e29e8d90b013af66
Harry Potter <fbef0102@gmail.com>
2020-06-22 17:11:54 +0800

    2020_6_22 updated

---
+++ HW9 AR_Game/New Unity Project 9/Assets/Scripts/CamFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour {
+	Vector3 offest;
+	public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+		offest = this.transform.position - target.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = target.transform.position+offest;
+	}
+}
